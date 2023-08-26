864b61ea43e654367919d497c8e8623b5327adad
Ax51 <80718359+Ax51@users.noreply.github.com>
2023-08-26 16:31:34 +0300

    feat: added additional keymappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -55,3 +55,28 @@ vim.keymap.set("n", "gd", telescope.lsp_definitions, { noremap = true, silent = 
 
 -- LazyGit
 vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", {})
+
+-- Navigation
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<leader>/", "gcc")
+
+-- Splits
+vim.keymap.set("n", "|", ":vsplit<CR>")
+vim.keymap.set("n", "\\", ":split<CR>")
+
+-- Other
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("n", "<leader>Q", ":qall<CR>")
+vim.keymap.set("n", "<leader>x", ":BufferLinePickClose<CR>")
+vim.keymap.set("n", "<leader>X", ":BufferLineCloseRight<CR>")
+vim.keymap.set("n", "<leader>s", ":BufferLineSortByTabs<CR>")
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+-- Tabs
+vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<s-Tab>", ":BufferLineCyclePrev<CR>")
