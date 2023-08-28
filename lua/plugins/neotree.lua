b3b8bb510423b52cc254579bc2338941461f2dec
Ax51 <80718359+Ax51@users.noreply.github.com>
2023-08-28 23:14:05 +0300

    refactor: hide .ds_store files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,18 +1,18 @@
-vim.fn.sign_define("DiagnosticSignError",
-  {text = " ", texthl = "DiagnosticSignError"})
-vim.fn.sign_define("DiagnosticSignWarn",
-  {text = " ", texthl = "DiagnosticSignWarn"})
-vim.fn.sign_define("DiagnosticSignInfo",
-  {text = " ", texthl = "DiagnosticSignInfo"})
-vim.fn.sign_define("DiagnosticSignHint",
-  {text = "󰌵", texthl = "DiagnosticSignHint"})
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
 require("neo-tree").setup({
-    filesystem = {
-        filtered_items = {
-            visible = true,
-            hide_dotfiles = false,
-            hide_gitignored = true,
-        }
-    }
+	filesystem = {
+		filtered_items = {
+			visible = true,
+			hide_dotfiles = false,
+			hide_gitignored = true,
+			never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+				".DS_Store",
+				"thumbs.db",
+			},
+		},
+	},
 })
