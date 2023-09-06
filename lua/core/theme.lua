70a16b01da48dbca6c2e11d60dac0e734c1a135c
Ax51 <80718359+Ax51@users.noreply.github.com>
2023-09-06 18:43:37 +0300

    feat: set default colortheme to ayu-mirage

--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -1,5 +1,7 @@
 vim.opt.termguicolors = true
 -- vim.cmd.colorscheme("kanagawa")
-vim.cmd.colorscheme("darkplus")
+vim.cmd.colorscheme("ayu-mirage")
+-- vim.cmd.colorscheme("darkplus")
+
 vim.cmd([[hi StatusLine guifg=#50bc43]])
 vim.cmd([[hi EndOfBuffer guifg=#4c4c4c]])
