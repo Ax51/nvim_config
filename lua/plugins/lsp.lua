eec52645b6f95d97b6c52f22ed68095a44afef97
Ax51 <80718359+Ax51@users.noreply.github.com>
2023-09-08 13:34:14 +0300

    feat: enabled lua_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,33 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 -- Sntup language servers.
 local lspconfig = require("lspconfig")
 lspconfig.pyright.setup({})
+lspconfig.lua_ls.setup({
+	on_init = function(client)
+		local path = client.workspace_folders[1].name
+		if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+			client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+				Lua = {
+					runtime = {
+						version = "LuaJIT",
+					},
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+							-- "${3rd}/luv/library"
+							-- "${3rd}/busted/library",
+						},
+						-- NOTE: or pull in all of 'runtimepath'. But this is a lot slower
+						-- library = vim.api.nvim_get_runtime_file("", true),
+					},
+				},
+			})
+
+			client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+		end
+		return true
+	end,
+})
 lspconfig.tsserver.setup({
 	on_attach = function(client, bufnr)
 		if client.server_capabilities.documentSymbolProvider then
