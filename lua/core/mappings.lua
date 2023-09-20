18b041e3a4505a44fc14b7ad099c24b79616a021
Ax51 <80718359+Ax51@users.noreply.github.com>
2023-09-20 16:46:45 +0300

    feat: added <leader>C script

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -91,7 +91,23 @@ vim.keymap.set("n", "<leader>s", ":BufferLineSortByTabs<CR>")
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.keymap.set("n", "<leader>rs", ":!bun %<CR>")
-vim.keymap.set("v", "<leader>c", ":'<,'>t'><CR>") -- copy selected lines right below
+vim.keymap.set("v", "<leader>c", ":'<,'>t'><CR>") -- NOTE: copy selected lines and paste them below
+vim.keymap.set("v", "<leader>C", function()
+	-- NOTE: copy selected lines, paste them below and comment repeated lines
+	local comApi = require("Comment.api")
+	local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+	vim.api.nvim_feedkeys(esc, "nx", false)
+
+	local initialCursorPos = vim.api.nvim_win_get_cursor(0)
+	local startLine = vim.fn.getpos("'<")[2]
+	local endLine = vim.fn.getpos("'>")[2]
+	local diffLines = math.abs(startLine - endLine)
+
+	vim.cmd("'<,'>t'>")
+	vim.api.nvim_win_set_cursor(0, { endLine + 1, 0 })
+	comApi.comment.linewise.count(diffLines + 1)
+	vim.api.nvim_win_set_cursor(0, initialCursorPos)
+end)
 
 -- Tabs
 vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
