63490824df2aa606205f0d2e66743de27312ff49
Ax51 <80718359+Ax51@users.noreply.github.com>
2023-09-28 21:29:34 +0300

    chore: updated plugins (freeze indent-blankline)

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -1,5 +1,14 @@
+-- NOTE: v3 has some breaking changes and works different, need more info
+-- return {
+-- 	"lukas-reineke/indent-blankline.nvim",
+-- 	main = "ibl",
+-- 	event = { "BufReadPost", "BufNewFile" },
+--
+-- 	config = true,
+-- }
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	version = "2.20.8",
 	event = { "BufReadPost", "BufNewFile" },
 
 	config = function()
