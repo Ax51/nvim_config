9ed2bab3f44d269a7dd6cc6ed493a688a2e1113e
Ax51 <80718359+Ax51@users.noreply.github.com>
2023-10-04 15:14:27 +0300

    feat: migrated to v3 of indent-blankline plugin

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -1,19 +1,15 @@
--- NOTE: v3 has some breaking changes and works different, need more info
--- return {
--- 	"lukas-reineke/indent-blankline.nvim",
--- 	main = "ibl",
--- 	event = { "BufReadPost", "BufNewFile" },
---
--- 	config = true,
--- }
 return {
 	"lukas-reineke/indent-blankline.nvim",
-	version = "2.20.8",
+	main = "ibl",
 	event = { "BufReadPost", "BufNewFile" },
 
 	config = function()
-		require("indent_blankline").setup({
-			show_current_context = true,
+		require("ibl").setup({
+			scope = {
+				char = "▏",
+				show_start = false,
+				show_end = false,
+			},
 		})
 	end,
 }
