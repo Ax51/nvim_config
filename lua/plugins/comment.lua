a2577e8d165ff2a45edd35de1b0da7a4208616e2
Ax51 <80718359+Ax51@users.noreply.github.com>
2023-11-20 16:31:53 +0300

    feat: removed comment remappings for more options to comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -4,12 +4,6 @@ return {
 
 	config = function()
 		require("Comment").setup({
-			-- TODO: change comments highlight colors
-			-- TODO: check why some togglers here, and some - in mappings file
-			toggler = {
-				line = "<leader>/",
-				block = "<leader>?",
-			},
 			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 		})
 	end,
