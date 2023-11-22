baf8602bc1d15539f87941c60df3ac21338b7edc
Ax51 <80718359+Ax51@users.noreply.github.com>
2023-11-22 16:45:30 +0300

    refactor: removed unused vscode snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,10 +1,8 @@
 return {
 	"L3MON4D3/LuaSnip",
 	lazy = true,
-	dependencies = { "rafamadriz/friendly-snippets" },
 
 	config = function()
-		require("luasnip.loaders.from_vscode").lazy_load()
 		-- NOTE: my custom snippets
 		require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "./snippets" } })
 	end,
