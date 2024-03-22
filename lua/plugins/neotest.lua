ba94fbc2a095e7b6c62e13afdf243fdde92f423e
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-03-22 17:48:26 +0300

    fix: added missing nvim-nio plugin for new version of neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,9 +1,11 @@
 return {
 	"nvim-neotest/neotest",
 	dependencies = {
+		"nvim-neotest/nvim-nio",
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 		"antoinemadec/FixCursorHold.nvim",
+
 		"haydenmeade/neotest-jest",
 		"thenbe/neotest-playwright",
 	},
