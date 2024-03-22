28f6b4896c4dc269d9a304a40b52674ff90285ce
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-03-22 15:00:32 +0300

    feat: added taplo lspserver for toml files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,6 +13,7 @@ return {
 		-- Setup language servers.
 		local lspconfig = require("lspconfig")
 		lspconfig.mdx_analyzer.setup({})
+		lspconfig.taplo.setup({})
 		lspconfig.lua_ls.setup({
 			on_init = function(client)
 				local path = client.workspace_folders[1].name
