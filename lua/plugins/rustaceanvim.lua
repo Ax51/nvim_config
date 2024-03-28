328570ad980427e297d59a3002cf06ea49a45d3d
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-03-28 13:06:00 +0300

    feat: added rust specific keymaps

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,3 +1,16 @@
+local nmap = unpack(require("utils.keymappings"))
+
+vim.g.rustaceanvim = function()
+	return {
+		server = {
+			on_attach = function()
+				nmap("<leader>a", ":RustLsp codeAction<CR>")
+				nmap("<leader>k", ":RustLsp openDocs<CR>")
+			end,
+		},
+	}
+end
+
 return {
 	"mrcjkb/rustaceanvim",
 	-- version = "^4",
