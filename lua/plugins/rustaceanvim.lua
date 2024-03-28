467d3e27066533567f43df1374609e27ae235a09
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-03-28 13:43:02 +0300

    refactor: moved rust specific keymaps asignments to init cb

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,18 +1,20 @@
-local nmap = unpack(require("utils.keymappings"))
-
-vim.g.rustaceanvim = function()
-	return {
-		server = {
-			on_attach = function()
-				nmap("<leader>a", ":RustLsp codeAction<CR>")
-				nmap("<leader>k", ":RustLsp openDocs<CR>")
-			end,
-		},
-	}
-end
-
 return {
 	"mrcjkb/rustaceanvim",
 	-- version = "^4",
 	ft = "rust",
+
+	init = function()
+		local nmap = unpack(require("utils.keymappings"))
+
+		vim.g.rustaceanvim = function()
+			return {
+				server = {
+					on_attach = function()
+						nmap("<leader>a", ":RustLsp codeAction<CR>")
+						nmap("<leader>k", ":RustLsp openDocs<CR>")
+					end,
+				},
+			}
+		end
+	end,
 }
