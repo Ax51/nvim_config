c50a2e761f139109d178d6894fba263bd8e9c720
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-04-22 17:00:08 +0300

    feat: updated lualine sections

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -27,18 +27,19 @@ return {
 			},
 			sections = {
 				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_b = { "diagnostics" },
 				lualine_c = {
 					{ "filename", path = 1 },
 				},
-				lualine_x = {},
-				lualine_y = { "progress" },
+				lualine_x = { "progress" },
+				lualine_y = { "diff" },
 				lualine_z = { "location" },
 			},
 			inactive_sections = {
 				lualine_a = {},
 				lualine_b = {},
 				lualine_c = {
+					"diagnostics",
 					{
 						"filename",
 						path = 1,
