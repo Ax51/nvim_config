067af04ddc00fa48116de0d794a3ec708b72d109
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-04-27 18:28:08 +0300

    feat: attached navic & navbuddy to rustaceanvim

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,20 +1,25 @@
 return {
-	"mrcjkb/rustaceanvim",
-	-- version = "^4",
-	ft = "rust",
+  "mrcjkb/rustaceanvim",
+  -- version = "^4",
+  ft = "rust",
 
-	init = function()
-		local nmap = unpack(require("utils.keymappings"))
+  init = function()
+    local nmap = unpack(require("utils.keymappings"))
 
-		vim.g.rustaceanvim = function()
-			return {
-				server = {
-					on_attach = function()
-						nmap("<leader>a", ":RustLsp codeAction<CR>")
-						nmap("<leader>k", ":RustLsp openDocs<CR>")
-					end,
-				},
-			}
-		end
-	end,
+    vim.g.rustaceanvim = function()
+      return {
+        server = {
+          on_attach = function(client, bufnr)
+            if client.server_capabilities.documentSymbolProvider then
+              require("nvim-navic").attach(client, bufnr)
+              require("nvim-navbuddy").attach(client, bufnr)
+            end
+
+            nmap("<leader>a", ":RustLsp codeAction<CR>")
+            nmap("<leader>k", ":RustLsp openDocs<CR>")
+          end,
+        },
+      }
+    end
+  end,
 }
