d165f33c233e52a47e6b346259e0d3963ff93218
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-06-05 19:21:50 +0300

    refactor: auto format util whitespaces

--- lua/utils/close_all_bufs_ex_curr.lua
+++ lua/utils/close_all_bufs_ex_curr.lua
@@ -1,12 +1,12 @@
 local closeAllBufsExceptCurrent = function() -- delete all buffers, expect active one
-	local rawBufArr = vim.api.nvim_list_bufs()
-	local currBufNum = vim.api.nvim_get_current_buf()
+  local rawBufArr = vim.api.nvim_list_bufs()
+  local currBufNum = vim.api.nvim_get_current_buf()
 
-	for _, bufnr in ipairs(rawBufArr) do
-		if bufnr ~= currBufNum and vim.fn.buflisted(bufnr) and not vim.api.nvim_buf_get_option(bufnr, "modified") then
-			vim.api.nvim_buf_delete(bufnr, {})
-		end
-	end
+  for _, bufnr in ipairs(rawBufArr) do
+    if bufnr ~= currBufNum and vim.fn.buflisted(bufnr) and not vim.api.nvim_buf_get_option(bufnr, "modified") then
+      vim.api.nvim_buf_delete(bufnr, {})
+    end
+  end
 end
 
 return closeAllBufsExceptCurrent
