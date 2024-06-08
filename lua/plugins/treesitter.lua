17c69b26d81e4fff10c19c3d0ddf8ca7caf4828b
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-06-08 15:26:06 +0300

    fix: linter warn about missing nvim-treesitter config fields

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,8 @@ return {
     require("nvim-treesitter.configs").setup({
       ensure_installed = "all",
       sync_install = false,
+      ignore_install = {},
+      modules = {},
       auto_install = true,
       highlight = {
         enable = true,
