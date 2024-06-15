c8cd6d28dbc0cdbf5cac5688e3ed1e6579069bb6
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-06-15 17:09:26 +0300

    docs: added types for remappings utils

--- lua/utils/keymappings.lua
+++ lua/utils/keymappings.lua
@@ -1,5 +1,10 @@
 local M = {}
 
+---full controll over remappings
+---@param shortcut string new keys combination to add
+---@param command string|function command to execute. can be a Lua function.
+---@param mode string|string[] Mode short-name, see |nvim_set_keymap()|.
+---@param opts? vim.keymap.set.Opts
 M.remap = function(shortcut, command, mode, opts)
   local defmode = "n"
   local defopts = { silent = true }
@@ -7,6 +12,10 @@ M.remap = function(shortcut, command, mode, opts)
   vim.keymap.set(mode or defmode, shortcut, command, opts or defopts)
 end
 
+---fast and easy remappings for normal mode only
+---@param shortcut string new keys combination to add
+---@param command string|function command to execute. can be a Lua function.
+---@param opts? vim.keymap.set.Opts
 M.nmap = function(shortcut, command, opts)
   M.remap(shortcut, command, "n", opts)
 end
