8cb3487cb23c06855e03bc9bf9d203982d0c78d8
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-06-15 16:42:34 +0300

    feat: switched octo from telescope to fzf-lua

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -2,12 +2,13 @@ return {
   'pwntester/octo.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim',
+    'ibhagwan/fzf-lua',
     'nvim-tree/nvim-web-devicons',
   },
   cmd = "Octo",
 
   opts = {
+    picker = "fzf-lua",
     suppress_missing_scope = {
       projects_v2 = true,
     }
