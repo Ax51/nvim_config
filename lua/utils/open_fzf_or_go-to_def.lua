0b4a348e4afb2be77a9aa570b0514470e14859b5
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-06-18 10:39:45 +0300

    feat: added `open_fzf_or_go-to_def` util
    If there is only single `lsp_definition` - go-to this definition
    instantly. But if there are several definitions - open `FzfLua
    lsp_definitions` instead

---
+++ lua/utils/open_fzf_or_go-to_def.lua
@@ -0,0 +1,19 @@
+return function()
+  local function handle_definition(_, result)
+    if not result or vim.tbl_isempty(result) then
+      print("No definitions found")
+      return
+    end
+
+    if #result == 1 then
+      -- NOTE: Single definition found
+      vim.lsp.buf.definition()
+    else
+      -- NOTE: Multiple definitions found
+      require('fzf-lua').lsp_definitions()
+    end
+  end
+
+  local params = vim.lsp.util.make_position_params()
+  vim.lsp.buf_request(0, 'textDocument/definition', params, handle_definition)
+end
