dd2a60c92b2b0f0f8855763fcfc67d5c4a493ef8
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-08-07 19:55:47 +0300

    fix: colors for `package-info` with lazy loading

--- lua/plugins/js-package-info.lua
+++ lua/plugins/js-package-info.lua
@@ -13,4 +13,13 @@ return {
       outdated = "#d19a66",
     },
   },
+
+  config = function(_, opts)
+    require("package-info").setup(opts)
+
+    -- NOTE: temporary fix to set colors with lazy loading this plugin due to this bug:
+    -- https://github.com/vuki656/package-info.nvim/issues/155
+    vim.cmd([[highlight PackageInfoUpToDateVersion guifg=]] .. opts.colors.up_to_date)
+    vim.cmd([[highlight PackageInfoOutdatedVersion guifg=]] .. opts.colors.outdated)
+  end
 }
