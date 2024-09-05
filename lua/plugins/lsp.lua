ca569c08512a856777d35ab28abe9a8bb8646e50
Ax51 <80718359+Ax51@users.noreply.github.com>
2024-09-05 11:52:30 +0300

    chore!: updated lsp plugin & config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -107,7 +107,7 @@ return {
       capabilities = capabilities,
     })
 
-    lspconfig.tsserver.setup({
+    lspconfig.ts_ls.setup({
       on_attach = function(client, bufnr)
         if client.server_capabilities.documentSymbolProvider then
           require("nvim-navic").attach(client, bufnr)
