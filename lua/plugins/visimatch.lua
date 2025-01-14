26258d5c8a839b1474c75cf147d8b169010813fc
Ax51 <80718359+Ax51@users.noreply.github.com>
2025-01-14 17:48:02 +0300

    feat: added lazy loading for `visimatch`

--- lua/plugins/visimatch.lua
+++ lua/plugins/visimatch.lua
@@ -1,7 +1,9 @@
 return {
   "wurli/visimatch.nvim",
+  keys = { "v", "V" },
   opts = {
     -- NOTE: check for `lua/themes/init.lua > hl.VisimatchOthers`
-    hl_group = "VisimatchOthers"
+    hl_group = "VisimatchOthers",
+    chars_lower_limit = 4,
   }
 }
