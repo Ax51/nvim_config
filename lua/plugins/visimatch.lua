249911fb603fe5271e0032948ab84c7c1b479171
Ax51 <80718359+Ax51@users.noreply.github.com>
2025-01-16 15:06:52 +0300

    !feat: got rid of `visimatch` because of `pattern too complex` lib issue

--- lua/plugins/visimatch.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "wurli/visimatch.nvim",
-  keys = { "v", "V" },
-  opts = {
-    -- NOTE: check for `lua/themes/init.lua > hl.VisimatchOthers`
-    hl_group = "VisimatchOthers",
-    chars_lower_limit = 4,
-  }
-}
