928e1aed903e1976c87e1ffbad2722fff177e67c
Ax51 <80718359+Ax51@users.noreply.github.com>
2025-03-22 11:48:04 +0300

    feat: added markdown render for copilot answers

--- lua/plugins/render-md.lua
+++ lua/plugins/render-md.lua
@@ -1,7 +1,7 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-  ft = "markdown",
+  ft = { "markdown", "copilot-chat" },
 
   ---@module "render-markdown"
   ---@type render.md.UserConfig
