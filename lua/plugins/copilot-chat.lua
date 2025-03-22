62e4d559bf148d8aeb6007470a10a997ab4e58bf
Ax51 <80718359+Ax51@users.noreply.github.com>
2025-03-22 13:41:35 +0300

    feat: Add detailed configuration for CopilotChat.nvim

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -4,7 +4,41 @@ return {
     "zbirenbaum/copilot.lua",
     "nvim-lua/plenary.nvim",
   },
-  cmd = { "CopilotChat", "Copilot" },
+  cmd = {
+    "Copilot",
+    "CopilotChat",
+    "CopilotChatPrompt",
+  },
   build = "make tiktoken",
-  opts = true,
+  config = function()
+    local chat = require("CopilotChat");
+    local prompts = require("CopilotChat.config.prompts");
+
+    chat.setup({
+      prompts = {
+        CommitMsg = {
+          prompt =
+          "Please provide me a list of at most 10 suggestions for commit message according to the currently staged changes.",
+          system_prompt = prompts.COPILOT_BASE.system_prompt .. [[
+The commit message must be prefixed with `!` since this is a trigger for my commit hook.
+Commit must starts with one of the following key words:
+-feat
+-fix
+-chore
+-docs
+-style
+-refactor
+-perf
+-test
+after key word please paste `:` that will indicate the start of the commit message.]],
+          context = "git:staged",
+          description = "List of 10 commit messages for staged changes",
+        }
+      },
+      highlight_headers = false,
+      separator = '---',
+      error_header = '> [!ERROR] Error',
+    })
+  end
+  ,
 }
