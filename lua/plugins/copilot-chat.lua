4bf8ffe85043bad376ce21ef8ea0bf7ac3fb0e35
Ax51 <80718359+Ax51@users.noreply.github.com>
2025-03-22 11:17:36 +0300

    refactor: removed unused comments

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -5,7 +5,6 @@ return {
     "nvim-lua/plenary.nvim",
   },
   cmd = { "CopilotChat", "Copilot" },
-  build = "make tiktoken", -- Only on MacOS or Linux
+  build = "make tiktoken",
   opts = true,
-  -- See Commands section for default commands if you want to lazy load on them
 }
