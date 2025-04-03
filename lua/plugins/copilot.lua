433b539c1f85ed7984e68fe3878af57050178d8e
Ax51 <80718359+Ax51@users.noreply.github.com>
2025-04-03 12:14:55 +0300

    feat: keymapping to accept only single line for copilot suggestion

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -7,6 +7,9 @@ return {
     suggestion = {
       enabled = true,
       auto_trigger = true,
+      keymap = {
+        accept_line = "<M-L>",
+      }
     },
     copilot_model = "gpt-4o-copilot",
   },
