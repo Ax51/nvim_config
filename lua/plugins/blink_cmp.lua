cd358d6db1a10ee9997d49683e34e0a736199191
Ax51 <80718359+Ax51@users.noreply.github.com>
2025-05-13 14:00:40 +0300

    !refactor: reduced mess in `blink_cmp` plugin config. Still integration WIP

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -18,52 +18,25 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
       preset = 'default',
-      ['<Tab>'] = {
-        function(cmp)
-          if cmp.is_ghost_text_visible() and not cmp.is_menu_visible() then return cmp.accept() end
-        end,
-        'show_and_insert',
-        'select_next',
-      },
-      ['<S-Tab>'] = { 'show_and_insert', 'select_prev' }, -- ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-      ['<CR>'] = { 'select_and_accept', "fallback" },
-
-      -- ['<C-x>'] = { 'hide' },
       --
-      -- ['<Tab>'] = {
-      --   function(cmp)
-      --     if cmp.snippet_active() then
-      --       return cmp.accept()
-      --     else
-      --       return cmp.select_and_accept()
-      --     end
-      --   end,
-      --   'snippet_forward',
-      --   'fallback'
-      -- },
-      -- ['<S-Tab>'] = { 'select_prev', 'fallback' },
-      -- ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+      -- NOTE: Default preset keymaps:
+      --
+      -- ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      -- ['<C-e>'] = { 'hide' },
+      -- ['<C-y>'] = { 'select_and_accept' },
+      --
+      -- ['<Up>'] = { 'select_prev', 'fallback' },
+      -- ['<Down>'] = { 'select_next', 'fallback' },
       -- ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+      -- ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
       --
       -- ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
       -- ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
       --
-      -- -- ['<Tab>'] = { 'snippet_forward', 'fallback' },
-      -- -- ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+      -- ['<Tab>'] = { 'snippet_forward', 'fallback' },
+      -- ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
       --
       -- ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
     },
