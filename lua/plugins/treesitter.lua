d93c05787b85d36d326f7403dc29c523946ecb00
Ax51 <80718359+Ax51@users.noreply.github.com>
2025-06-05 11:13:50 +0300

    feat!: switch to use `main` branch for treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,17 +3,34 @@ return {
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
 
+  branch = "main",
+
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = "all",
-      sync_install = false,
-      ignore_install = {},
-      modules = {},
-      auto_install = true,
-      highlight = {
-        enable = true,
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "rust",
+        "javascript",
+        "typescript",
+        "typescriptreact",
+        "javascriptreact",
+        "lua",
+        "python",
+        "go",
+        "sh",
+        "markdown",
+        "mdx",
+        "yaml",
+        "json",
+        "jsonc",
       },
+      callback = function()
+        -- NOTE: syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+        -- NOTE: folds, provided by Neovim
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        -- NOTE: indentation, provided by nvim-treesitter
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
-    vim.treesitter.language.register("markdown", { "mdx", "markdown", "markdown.mdx" })
-  end,
+  end
 }
