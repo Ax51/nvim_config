fd99b17f29329ff53f8eb8c9f8096b1a9d6cf4c2
Ax51 <80718359+Ax51@users.noreply.github.com>
2025-09-23 12:00:53 +0300

    !fix: upgraded `package-info` plugin that fixes colors override

--- lua/plugins/js-package-info.lua
+++ lua/plugins/js-package-info.lua
@@ -7,19 +7,5 @@ return {
   -- NOTE: with lazy-loading somewhy badges colors are missing
   event = "BufEnter package.json",
 
-  opts = {
-    colors = {
-      up_to_date = "#3C4048",
-      outdated = "#d19a66",
-    },
-  },
-
-  config = function(_, opts)
-    require("package-info").setup(opts)
-
-    -- NOTE: temporary fix to set colors with lazy loading this plugin due to this bug:
-    -- https://github.com/vuki656/package-info.nvim/issues/155
-    vim.cmd([[highlight PackageInfoUpToDateVersion guifg=]] .. opts.colors.up_to_date)
-    vim.cmd([[highlight PackageInfoOutdatedVersion guifg=]] .. opts.colors.outdated)
-  end
+  opts = true,
 }
